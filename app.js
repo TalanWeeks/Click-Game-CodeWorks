69c05c7bee1f3a466eb3385b2b2a5770807fe9ae
Talan Weeks <talan.jakoby.weeks@gmail.com>
2021-08-28 15:13:41 -0600

    added scrolling cards and some DOM manipulation

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+// variables
 
+let currentCheeseMined = 0;
+let 
+
+
+
+// functions
 function helloWorld(){
-  console.log("hello")
+  currentCheeseMined +=1
+  console.log(currentCheeseMined)
+
+  updateInventory()
+
+}
+
+function updateInventory(){
+  document.getElementById('current-cheese-total').innerHTML = currentCheeseMined;
 }
