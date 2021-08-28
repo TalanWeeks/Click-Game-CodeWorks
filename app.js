ea1628c452e0207bcae7c7567298e03656200f4d
Talan Weeks <talan.jakoby.weeks@gmail.com>
2021-08-28 16:10:17 -0600

    live updating inventory

--- app.js
+++ app.js
@@ -1,19 +1,97 @@
 // variables
 
 let currentCheeseMined = 0;
-let 
+let totalCheeseEarned = 0;
+let currentCartTotal = 0;
+let currentRoverTotal = 0;
+let currentCartStyle = '';
+let currentRoverStyle = '';
+let currentPickStyle = '';
+
+// Dictionaries
+
+let clickUpgrades = {
+  bareHands: {
+    name:'Bare Hands',
+    cost: 0,
+    quantity: 0,
+    multiplier: 1
+  },
+  pickAxe: {
+    name: 'Pick Axe',
+    cost:10,
+    quantity:0,
+    multiplier: 5
+  },
+  superSaiyan: {
+    name: 'Super Saiyan Mode',
+    cost: 30,
+    quantity:0,
+    multiplier: 20
+  }
+}
+
+let cartUpgrades = {
+  bucket: {
+    name:'Bucket',
+    cost:0,
+    quantity: 50,
+  },
+  mineCart: {
+    name:'Mine Cart',
+    cost: 100,
+    quantity: 100,
+  },
+  dumpTruck: {
+    name:'Dump Truck',
+    cost:400,
+    quantity: 200,
+  }
+}
+
+let rover = {
+  name: 'Moon Rover',
+  cost: 100,
+  quantity: 0, 
+}
+
+
+
+
 
 
 
 // functions
-function helloWorld(){
+
+// NOTE adding to current cheese when moon is clicked
+function mineCheese(){
+  totalCheeseEarned += 1
   currentCheeseMined +=1
+  currentCartTotal += 1
+
   console.log(currentCheeseMined)
+
 
   updateInventory()
 
 }
 
+// NOTE keeping track of total cheese to manipulate later
+
+
 function updateInventory(){
-  document.getElementById('current-cheese-total').innerHTML = currentCheeseMined;
+  
+   document.getElementById('current-cheese-total').innerHTML = currentCheeseMined;
+
+   document.getElementById('total-cheese-earned').innerHTML = totalCheeseEarned;
+
+  document.getElementById('current-click-style').innerText = clickUpgrades.bareHands.name;
+
+  document.getElementById('current-cart-style').innerText = cartUpgrades.bucket.name;
+
+   document.getElementById('current-cart-total').innerHTML = currentCartTotal;
+
+   document.getElementById('current-rover-total').innerHTML = currentRoverTotal;
+  
 }
+updateInventory()
